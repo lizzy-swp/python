ce5c70bfe14735d4ef8b9f96f4bcecdf003e9190
lizzy <1710337449@qq.com>
2019-12-12 11:54:05 +0800

    zifuchuan

---
+++ 字符串.py
@@ -0,0 +1,13 @@
+mes="hi girl"
+print(mes.title()) #首字母大写
+print(mes.upper()) #字母全部大写
+print(mes.lower()) #字母全部小写
+name="lizzy"
+full=mes+" "+name #字符串的拼接
+print(full)
+print("\tpython") #\t=tab
+print("hallo:\nppp\nccc") #\n=next line
+mmm="  python  "
+print(mmm.rstrip()) #输出时去掉字符串中右边多余的空格
+print(mmm.strip()) #输出时去掉字符串中两边多余的空格
+print(mmm.lstrip()) #输出时去掉字符串中左边多余的空格
